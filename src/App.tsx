6972d18a06d12b554708d2c1b8875d17ca9cb82e
Semih Irmak <smhirmak@hotmail.com>
2023-10-11 21:42:22 +0300

    add websocket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [btc, setBtc] = useState<any>(null);
+  const [ltc, setLtc] = useState<any>(null);
 
-  return;
+  useEffect(() => {
+    const ws = new WebSocket('wss://ws.okx.com:8443/ws/v5/public');
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          op: 'subscribe',
+          args: [
+            {
+              channel: 'tickers',
+              instId: 'BTC-USDT'
+            },
+            {
+              channel: 'tickers',
+              instId: 'LTC-USDT'
+            }
+          ]
+        })
+      );
+    };
+    ws.onmessage = (event) => {
+      const variable = JSON.parse(event.data);
+      variable.arg.instId === 'BTC-USDT' ? setBtc(variable) : setLtc(variable);
+    };
+    return () => {
+      ws.close();
+    };
+  }, []);
+  // btc && console.log(btc?.data?.[0].last);
+  // ltc && console.log(ltc?.data?.[0].last);
+  return (
+    <div className="App">
+      <h1>OKX API</h1>
+      <p>{btc && btc?.data?.[0].last}</p>
+    </div>
+  );
 }
 
 export default App;
